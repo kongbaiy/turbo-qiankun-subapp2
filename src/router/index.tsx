94c48b57b182920dbe5c904b25e27502c874a065
kebi <kanglu-email@qq.com>
2026-08-20 11:17:45 +0800

    feat: 路由path采用自定义basename实现基础路径，避免多应用之间跳转警告并失效 多标签缓存

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,173 +4,170 @@ import { KeepAlive } from 'react-activation'
 import { asyncComponent } from './async-component'
 import Auth from './auth'
 
-const routers = createBrowserRouter(
-    [
-        {
-            path: '/',
-            loader: () => replace('/application-components/application'),
-        },
-        {
-            path: '/application-components',
-            handle: {
-                icon: 'AppstoreFilled',
-                name: '应用组件管理',
-            },
-            children: [
-                {
-                    path: 'platforms-manage',
-                    handle: {
-                        name: '平台管理',
-                    },
-                    element: (
-                        <KeepAlive>
-                            <Auth>
-                                {asyncComponent(
-                                    () =>
-                                        import('@/pages/application-components/platform-manage'),
-                                )}
-                            </Auth>
-                        </KeepAlive>
-                    ),
-                },
-                {
-                    path: 'application',
-                    handle: {
-                        name: '应用管理',
-                    },
-                    element: (
-                        <KeepAlive>
-                            <Auth>
-                                {asyncComponent(
-                                    () =>
-                                        import('@/pages/application-components/application-manage'),
-                                )}
-                            </Auth>
-                        </KeepAlive>
-                    ),
-                },
-                {
-                    path: 'component',
-                    handle: {
-                        name: '组件管理',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/application-components/component-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-                {
-                    path: 'api',
-                    handle: {
-                        name: '接口管理',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/application-components/api-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-            ],
-        },
-        {
-            path: '/global-data-standard',
-            handle: {
-                icon: 'DatabaseFilled',
-                name: '全局数据标准',
-            },
-            children: [
-                {
-                    path: 'dict',
-                    handle: {
-                        name: '数据字典',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/global-data-standard/dict-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-                {
-                    path: 'dimension',
-                    handle: {
-                        name: '维度管理',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/global-data-standard/dimension-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-                {
-                    path: 'serial',
-                    handle: {
-                        name: '流水号管理',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/global-data-standard/serial-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-            ],
-        },
-        {
-            path: '/business-standard',
-            handle: {
-                icon: 'ToolFilled',
-                name: '业务标准定义',
-            },
-            children: [
-                {
-                    path: 'control',
-                    handle: {
-                        name: '控件管理',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/business-standard/control-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-                {
-                    path: 'form',
-                    handle: {
-                        name: '表单定义',
-                    },
-                    element: (
-                        <Auth>
-                            {asyncComponent(
-                                () =>
-                                    import('@/pages/business-standard/form-manage'),
-                            )}
-                        </Auth>
-                    ),
-                },
-            ],
-        },
-    ],
+const basename = '/test'
+
+const routers = createBrowserRouter([
     {
-        basename: '/test',
+        path: basename,
+        loader: () => replace(`${basename}/application-components/application`),
     },
-)
+    {
+        path: `${basename}/application-components`,
+        handle: {
+            icon: 'AppstoreFilled',
+            name: '应用组件管理',
+        },
+        children: [
+            {
+                path: 'platforms-manage',
+                handle: {
+                    name: '平台管理',
+                },
+                element: (
+                    <KeepAlive>
+                        <Auth>
+                            {asyncComponent(
+                                () =>
+                                    import('@/pages/application-components/platform-manage'),
+                            )}
+                        </Auth>
+                    </KeepAlive>
+                ),
+            },
+            {
+                path: 'application',
+                handle: {
+                    name: '应用管理',
+                },
+                element: (
+                    <KeepAlive>
+                        <Auth>
+                            {asyncComponent(
+                                () =>
+                                    import('@/pages/application-components/application-manage'),
+                            )}
+                        </Auth>
+                    </KeepAlive>
+                ),
+            },
+            {
+                path: 'component',
+                handle: {
+                    name: '组件管理',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/application-components/component-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+            {
+                path: 'api',
+                handle: {
+                    name: '接口管理',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/application-components/api-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+        ],
+    },
+    {
+        path: `${basename}/global-data-standard`,
+        handle: {
+            icon: 'DatabaseFilled',
+            name: '全局数据标准',
+        },
+        children: [
+            {
+                path: 'dict',
+                handle: {
+                    name: '数据字典',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/global-data-standard/dict-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+            {
+                path: 'dimension',
+                handle: {
+                    name: '维度管理',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/global-data-standard/dimension-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+            {
+                path: 'serial',
+                handle: {
+                    name: '流水号管理',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/global-data-standard/serial-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+        ],
+    },
+    {
+        path: `${basename}/business-standard`,
+        handle: {
+            icon: 'ToolFilled',
+            name: '业务标准定义',
+        },
+        children: [
+            {
+                path: 'control',
+                handle: {
+                    name: '控件管理',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/business-standard/control-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+            {
+                path: 'form',
+                handle: {
+                    name: '表单定义',
+                },
+                element: (
+                    <Auth>
+                        {asyncComponent(
+                            () =>
+                                import('@/pages/business-standard/form-manage'),
+                        )}
+                    </Auth>
+                ),
+            },
+        ],
+    },
+])
 
 export default routers
